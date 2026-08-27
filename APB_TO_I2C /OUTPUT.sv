e7be7c9c43cb502ef4876351955792b129bb55e8
mohammedirfanvlsi <mohammedirfan0719@gmail.com>
2026-08-27 13:15:05 +0530

    Create OUTPUT.sv

---
+++ APB_TO_I2C /OUTPUT.sv
@@ -0,0 +1,70 @@
+UVM_INFO @ 0: reporter [RNTST] Running test apb_test...
+UVM_INFO uvm_sequence.sv(16) @ 0: uvm_test_top.env.agent.sqr@@seq [SEQ] === STARTING APB-TO-I2C BRIDGE CONFIGURATION ===
+UVM_INFO uvm_sequence.sv(26) @ 85: uvm_test_top.env.agent.sqr@@seq [SEQ] === BRIDGE CONFIGURED. STARTING I2C TRANSFER ===
+UVM_INFO uvm_scoreboard.sv(34) @ 115: uvm_test_top.env.scb [SCB] CPU wants to send: b4
+UVM_INFO uvm_scoreboard.sv(46) @ 725745: uvm_test_top.env.scb [SCB] PASS! APB sent b4, and I2C output b4!
+UVM_INFO uvm_sequence.sv(46) @ 733825: uvm_test_top.env.agent.sqr@@seq [SEQ] === ADDRESS PHASE COMPLETE. SLAVE IS LISTENING ===
+UVM_INFO uvm_scoreboard.sv(34) @ 733855: uvm_test_top.env.scb [SCB] CPU wants to send: 8f
+UVM_INFO uvm_scoreboard.sv(46) @ 798105: uvm_test_top.env.scb [SCB] PASS! APB sent 8f, and I2C output 8f!
+UVM_INFO uvm_scoreboard.sv(34) @ 806215: uvm_test_top.env.scb [SCB] CPU wants to send: 3
+UVM_INFO uvm_scoreboard.sv(46) @ 870465: uvm_test_top.env.scb [SCB] PASS! APB sent 3, and I2C output 3!
+UVM_INFO uvm_scoreboard.sv(34) @ 878575: uvm_test_top.env.scb [SCB] CPU wants to send: ae
+UVM_INFO uvm_scoreboard.sv(46) @ 942825: uvm_test_top.env.scb [SCB] PASS! APB sent ae, and I2C output ae!
+UVM_INFO uvm_scoreboard.sv(34) @ 950935: uvm_test_top.env.scb [SCB] CPU wants to send: 91
+UVM_INFO uvm_scoreboard.sv(46) @ 1015185: uvm_test_top.env.scb [SCB] PASS! APB sent 91, and I2C output 91!
+UVM_INFO uvm_scoreboard.sv(34) @ 1023295: uvm_test_top.env.scb [SCB] CPU wants to send: ab
+UVM_INFO uvm_scoreboard.sv(46) @ 1087545: uvm_test_top.env.scb [SCB] PASS! APB sent ab, and I2C output ab!
+UVM_INFO uvm_scoreboard.sv(34) @ 1095655: uvm_test_top.env.scb [SCB] CPU wants to send: fc
+UVM_INFO uvm_scoreboard.sv(46) @ 1159905: uvm_test_top.env.scb [SCB] PASS! APB sent fc, and I2C output fc!
+UVM_INFO uvm_scoreboard.sv(34) @ 1168015: uvm_test_top.env.scb [SCB] CPU wants to send: 85
+UVM_INFO uvm_scoreboard.sv(46) @ 1232265: uvm_test_top.env.scb [SCB] PASS! APB sent 85, and I2C output 85!
+UVM_INFO uvm_scoreboard.sv(34) @ 1240375: uvm_test_top.env.scb [SCB] CPU wants to send: 45
+UVM_INFO uvm_scoreboard.sv(46) @ 1304625: uvm_test_top.env.scb [SCB] PASS! APB sent 45, and I2C output 45!
+UVM_INFO uvm_scoreboard.sv(34) @ 1312735: uvm_test_top.env.scb [SCB] CPU wants to send: 3d
+UVM_INFO uvm_scoreboard.sv(46) @ 1376985: uvm_test_top.env.scb [SCB] PASS! APB sent 3d, and I2C output 3d!
+UVM_INFO uvm_scoreboard.sv(34) @ 1385095: uvm_test_top.env.scb [SCB] CPU wants to send: 6c
+UVM_INFO uvm_scoreboard.sv(46) @ 1449345: uvm_test_top.env.scb [SCB] PASS! APB sent 6c, and I2C output 6c!
+UVM_INFO uvm_scoreboard.sv(34) @ 1457455: uvm_test_top.env.scb [SCB] CPU wants to send: d2
+UVM_INFO uvm_scoreboard.sv(46) @ 1521705: uvm_test_top.env.scb [SCB] PASS! APB sent d2, and I2C output d2!
+UVM_INFO uvm_scoreboard.sv(34) @ 1529815: uvm_test_top.env.scb [SCB] CPU wants to send: 42
+UVM_INFO uvm_scoreboard.sv(46) @ 1594065: uvm_test_top.env.scb [SCB] PASS! APB sent 42, and I2C output 42!
+UVM_INFO uvm_scoreboard.sv(34) @ 1602175: uvm_test_top.env.scb [SCB] CPU wants to send: 17
+UVM_INFO uvm_scoreboard.sv(46) @ 1666425: uvm_test_top.env.scb [SCB] PASS! APB sent 17, and I2C output 17!
+UVM_INFO uvm_scoreboard.sv(34) @ 1674535: uvm_test_top.env.scb [SCB] CPU wants to send: 23
+UVM_INFO uvm_scoreboard.sv(46) @ 1738785: uvm_test_top.env.scb [SCB] PASS! APB sent 23, and I2C output 23!
+UVM_INFO uvm_scoreboard.sv(34) @ 1746895: uvm_test_top.env.scb [SCB] CPU wants to send: 67
+UVM_INFO uvm_scoreboard.sv(46) @ 1811145: uvm_test_top.env.scb [SCB] PASS! APB sent 67, and I2C output 67!
+UVM_INFO uvm_scoreboard.sv(34) @ 1819255: uvm_test_top.env.scb [SCB] CPU wants to send: e2
+UVM_INFO uvm_scoreboard.sv(46) @ 1883505: uvm_test_top.env.scb [SCB] PASS! APB sent e2, and I2C output e2!
+UVM_INFO uvm_scoreboard.sv(34) @ 1891615: uvm_test_top.env.scb [SCB] CPU wants to send: 94
+UVM_INFO uvm_scoreboard.sv(46) @ 1955865: uvm_test_top.env.scb [SCB] PASS! APB sent 94, and I2C output 94!
+UVM_INFO uvm_scoreboard.sv(34) @ 1963975: uvm_test_top.env.scb [SCB] CPU wants to send: 7e
+UVM_INFO uvm_scoreboard.sv(46) @ 2028225: uvm_test_top.env.scb [SCB] PASS! APB sent 7e, and I2C output 7e!
+UVM_INFO uvm_scoreboard.sv(34) @ 2036335: uvm_test_top.env.scb [SCB] CPU wants to send: 72
+UVM_INFO uvm_scoreboard.sv(46) @ 2100585: uvm_test_top.env.scb [SCB] PASS! APB sent 72, and I2C output 72!
+UVM_INFO uvm_scoreboard.sv(34) @ 2108695: uvm_test_top.env.scb [SCB] CPU wants to send: ca
+UVM_INFO uvm_scoreboard.sv(46) @ 2172945: uvm_test_top.env.scb [SCB] PASS! APB sent ca, and I2C output ca!
+UVM_INFO uvm_scoreboard.sv(34) @ 2181055: uvm_test_top.env.scb [SCB] CPU wants to send: 59
+UVM_INFO uvm_scoreboard.sv(46) @ 2245305: uvm_test_top.env.scb [SCB] PASS! APB sent 59, and I2C output 59!
+UVM_INFO uvm_scoreboard.sv(34) @ 2253415: uvm_test_top.env.scb [SCB] CPU wants to send: 20
+UVM_INFO uvm_scoreboard.sv(46) @ 2317665: uvm_test_top.env.scb [SCB] PASS! APB sent 20, and I2C output 20!
+UVM_INFO uvm_scoreboard.sv(34) @ 2325775: uvm_test_top.env.scb [SCB] CPU wants to send: 1f
+UVM_INFO uvm_scoreboard.sv(46) @ 2390025: uvm_test_top.env.scb [SCB] PASS! APB sent 1f, and I2C output 1f!
+UVM_INFO uvm_scoreboard.sv(34) @ 2398135: uvm_test_top.env.scb [SCB] CPU wants to send: 55
+UVM_INFO uvm_scoreboard.sv(46) @ 2462385: uvm_test_top.env.scb [SCB] PASS! APB sent 55, and I2C output 55!
+UVM_INFO uvm_scoreboard.sv(34) @ 2470495: uvm_test_top.env.scb [SCB] CPU wants to send: 95
+UVM_INFO uvm_scoreboard.sv(46) @ 2534745: uvm_test_top.env.scb [SCB] PASS! APB sent 95, and I2C output 95!
+UVM_INFO uvm_scoreboard.sv(34) @ 2542855: uvm_test_top.env.scb [SCB] CPU wants to send: 39
+UVM_INFO uvm_scoreboard.sv(46) @ 2607105: uvm_test_top.env.scb [SCB] PASS! APB sent 39, and I2C output 39!
+UVM_INFO uvm_scoreboard.sv(34) @ 2615215: uvm_test_top.env.scb [SCB] CPU wants to send: 15
+UVM_INFO uvm_scoreboard.sv(46) @ 2679465: uvm_test_top.env.scb [SCB] PASS! APB sent 15, and I2C output 15!
+UVM_INFO uvm_scoreboard.sv(34) @ 2687575: uvm_test_top.env.scb [SCB] CPU wants to send: 28
+UVM_INFO uvm_scoreboard.sv(46) @ 2751825: uvm_test_top.env.scb [SCB] PASS! APB sent 28, and I2C output 28!
+UVM_INFO uvm_scoreboard.sv(34) @ 2759935: uvm_test_top.env.scb [SCB] CPU wants to send: 72
+UVM_INFO uvm_scoreboard.sv(46) @ 2824185: uvm_test_top.env.scb [SCB] PASS! APB sent 72, and I2C output 72!
+UVM_INFO uvm_scoreboard.sv(34) @ 2832295: uvm_test_top.env.scb [SCB] CPU wants to send: c7
+UVM_INFO uvm_scoreboard.sv(46) @ 2896545: uvm_test_top.env.scb [SCB] PASS! APB sent c7, and I2C output c7!
+UVM_INFO uvm_sequence.sv(73) @ 2904625: uvm_test_top.env.agent.sqr@@seq [SEQ] === ALL 10 TRANSFERS 100% COMPLETE! ===
+UVM_INFO /xcelium25.03/tools/methodology/UVM/CDNS-1.2/sv/src/base/uvm_objection.svh(1271) @ 2904625: reporter [TEST_DONE] 'run' phase is ready to proceed to the 'extract' phase
+UVM_INFO uvm_coverage.sv(61) @ 2904625: uvm_test_top.env.cov [COVERAGE] THE FINAL COVERAGE = 69.53 %
+UVM_INFO /xcelium25.03/tools/methodology/UVM/CDNS-1.2/sv/src/base/uvm_report_server.svh(851) @ 2904625: reporter [UVM/REPORT/SERVER] 
